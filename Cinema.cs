2e3932483baef32e70e9b8ffa16a9b5a45de0217
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-14 16:27:04 +0300

    Add files via upload

---
+++ Cinema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            
+            if(projection == "Premiere")
+            {
+                Console.WriteLine($"{12 * rows * columns:f2} leva");
+            }else if(projection == "Normal")
+            {
+                Console.WriteLine($"{7.50 * rows * columns:f2} leva");
+            }
+            else
+            {
+                Console.WriteLine($"{5 * rows * columns:f2} leva");
+            }
+        }
+    }
+}
